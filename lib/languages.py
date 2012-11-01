3b52673eab140f6b64aea2e7f5198cdf47b1a1d6
Eli Ribble <eribble@hirevue.com>
2012-11-01 22:49:22 +0000

    Add C++ support

--- lib/languages.py
+++ lib/languages.py
@@ -42,7 +42,7 @@ class Language(object):
         return self.profile.run(self, source, stdin, custom_timelimit=timelimit)
 
 class LanguageTest(object):
-    def __init__(self, name, language, source, stdout, stderr, returncode, error):
+    def __init__(self, name, language, source, stdout='', stderr='', returncode=0, error=None):
         self.name           = name
         self.language       = language
         self.source         = source
@@ -203,6 +203,57 @@ LanguageTest('test-includesafe', c,
     returncode  = 1,
     error       = 'compilation_error')
 
+cpp = Language('C++',
+    profile                     = exec_profiles.CompilerProfile(straitjacket_settings),
+    binary                      = 'g++',
+    apparmor_profile            = 'straitjacket/compiler/default',
+    compiled_apparmor_profile   = 'straitjacket/compiled/default',
+    filename                    = 'source.cpp')
+
+LanguageTest('test-simple', cpp,
+    source      = ( '#include <iostream>                                        \n'
+                    'int main(int argc, char** argv) {                          \n'
+                    '   std::cout << "compilation succeeded" << std::endl;      \n'
+                    '   return 0;                                               \n'
+                    '}'),
+    stdout      = 'compilation succeeded')
+
+LanguageTest('test-rlimit', cpp,
+    source      = ( '#include <iostream>                                        \n'
+                    'int main(int argc, char** argv) {                          \n'
+                    '   std::cout << "forked: " << fork() << std::endl;         \n'
+                    '   return 0;                                               \n'
+                    '}'),
+    stdout      = 'forked: -1')
+
+LanguageTest('test-apparmor', cpp,
+    source      = ( '#include <iostream>                                        \n'
+                    '#include <stdio.h>                                         \n'
+                    '#include <errno.h>                                         \n'
+                    'int main(int argc, char** argv) {                          \n'
+                    '   if(fopen("/etc/hosts", "r")) {                          \n'
+                    '       std::cout << "opened" << std::endl;                 \n'
+                    '   } else {                                                \n'
+                    '       std::cout << "not opened: " << errno << std::endl;  \n'
+                    '   }                                                       \n'
+                    '   return 0;                                               \n'
+                    '}'),
+    stdout      = 'not opened: 13')
+
+LanguageTest('test-argv0', cpp,
+    source      = ( '#include <iostream>                    \n'
+                    'int main(int argc, char** argv) {      \n'
+                    '   std::cout << argv[0] << std::endl;  \n'
+                    '   return 0;                           \n'
+                    '}'),
+    stdout      = 'straitjacket-binary')
+
+LanguageTest('test-includesafe', cpp,
+    source      = ( '#include "/etc/hosts"                          \n'
+                    'int main(int argc, char** argv) { return 0; }' ),
+    stderr      = 'fatal error: /etc/hosts: Permission denied',
+    returncode  = 1,
+    error       = 'compilation_error')
 
 def all():
     from lib import languages
