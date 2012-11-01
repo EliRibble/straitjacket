a8a5cb33d412a75fbdcc3866d4f9c47397687b8e
Eli Ribble <eribble@hirevue.com>
2012-11-01 19:40:30 +0000

    Better test formatting

--- lib/languages.py
+++ lib/languages.py
@@ -56,8 +56,18 @@ class LanguageTest(object):
         assert re.search(self.stderr_pattern, result.stderr)
 
 bash = Language('Bash', exec_profiles.InterpreterProfile(straitjacket_settings), binary='bash', filename='source.sh')
-LanguageTest('test-simple', bash, source='echo -n hello from bash', stdout='hello from bash', stderr='', returncode=0, error=None)
-LanguageTest('test-apparmor', bash, source='while read line; do echo -e "$line\n"; done < /etc/hosts', stdout='', stderr='line 2: /etc/hosts: Permission denied', returncode=1, error='runtime_error')
+LanguageTest('test-simple', bash,
+     source     = 'echo -n hello from bash',
+     stdout     = 'hello from bash',
+     stderr     = '',
+     returncode = 0,
+     error      = None)
+LanguageTest('test-apparmor', bash,
+     source     = 'while read line; do echo -e "$line\n"; done < /etc/hosts',
+     stdout     = '',
+     stderr     = 'line 2: /etc/hosts: Permission denied',
+     returncode = 1,
+     error      = 'runtime_error')
 
 def all():
     from lib import languages
