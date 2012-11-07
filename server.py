b70b6c5bfb381a1db2c24820415963511a4858b6
Eli Ribble <eribble@hirevue.com>
2012-11-06 21:18:16 -0700

    Fix indentation

--- server.py
+++ server.py
@@ -113,5 +113,5 @@ def webapp(wrapper=None, config_dir=DEFAULT_CONFIG_DIR, skip_language_checks=Fal
     return app
 
 if __name__ == "__main__":
-        webapp().run()
+    webapp().run()
 application = webapp().wsgifunc()
