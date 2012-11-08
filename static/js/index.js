a1e1748c51005f7f152483304ee1db66fbb45950
Eli Ribble <eribble@hirevue.com>
2012-11-08 18:31:37 +0000

    Now shows compilation and other output
    The returncode is shown as part of the status.
    This may be less than ideal, but I'll take it because
    it means less widgets on the page and is totally readable

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,27 @@
 function setup() {
-    var editor = ace.edit('editor');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/javascript');
-
     $.ajax({
         url: '/info',
         success: onGetLanguageInfo,
     });
+
+    editor = ace.edit('editor');
+    editor.setTheme('ace/theme/monokai');
+    editor.getSession().setMode('ace/mode/javascript');
+
+    $('#execute').click(onExecuteClick);
+    disableButton();
+}
+
+function disableButton() {
+    var $button = $('#execute');
+    $button.attr('disabled', 'disabled');
+    $button.addClass('disabled');
+}
+
+function enableButton() {
+    var $button = $('#execute');
+    $button.removeAttr('disabled');
+    $button.removeClass('disabled');
 }
 
 function onGetLanguageInfo(data, textStatus, jqXHR) {
@@ -16,6 +31,51 @@ function onGetLanguageInfo(data, textStatus, jqXHR) {
             $('<option></option>').val(language_name).html(info.visible_name)
         );
     });
+    enableButton();
 }
 
+function onExecuteClick() {
+    disableButton();
+    var source = editor.getValue();
+    var language = $('#language').val();
+    $.ajax({
+        type: 'POST',
+        url: '/execute',
+        success: onExecutionComplete,
+        data: {
+            language: language,
+            source: source
+        }
+    });
+}
+
+function onExecutionComplete(data, textStatus, jqXHR) {
+    $('#status').html(data.status);
+    $('#results_accordion').show();
+    if( 'compilation' in data ) {
+        $('.compilation').show();
+        $('#compilation-command').html(data.compilation.command);
+        $('#compilation-stdout').html(data.compilation.stdout);
+        $('#compilation-stderr').html(data.compilation.stderr);
+        $('#compilation-returncode').html(data.compilation.returncode);
+    } else {
+        $('.compilation').hide();
+    }
+    if( data.runs.length > 0 ) {
+        $('.stdout').show();
+        $('.stderr').show();
+        $('.returncode').show();
+        $('#stdout').html(data.runs[0].stdout);
+        $('#stderr').html(data.runs[0].stderr);
+        if( data.runs[0] != 0 ) {
+            $('#status').html($('#status').html() + ' (' + data.runs[0].returncode + ')');
+        }
+    } else {
+        $('.stdout').hide();
+        $('.stderr').hide();
+        $('.returncode').hide();
+    }
+
+    enableButton();
+}
 $(setup);
