5f842f275fe7a43446341b5084d33d3bf0c391b7
Eli Ribble <eribble@hirevue.com>
2012-11-09 21:14:41 +0000

    Fix the objective-c profile
    There were several things broken. I wasn't using the correct comilation flags
    provided by gnustep-config --objc-flags. Also the apparmor profile didn't allow
    for access to a few things that objective-c needs to run, like the /proc/
    portion of the filesystem.
    
    Ultimately we worked all of this out with a great deal of help from Pete Boden
    who came up with the source for the new language test I've added that shows
    that we can actually execute Objective-C code that reads stdin and prints to
    stdout, which we didn't have before

--- languages/objective_c.py
+++ languages/objective_c.py
@@ -5,9 +5,42 @@ import straitjacket_settings
 objective_c = Language('Objective-C',
     profile                     = exec_profiles.CompilerProfile(straitjacket_settings),
     binary                      = 'gcc',
-    compilation_command         = lambda source, compiler_file: ['gcc', source, '-lobjc', '-fconstant-string-class=NSConstantString', '-lgnustep-base', '-o', compiler_file],
+    compilation_command         = lambda source, compiler_file: ['gcc',
+         source,
+         '-MMD',
+         '-MP',
+         '-DGNUSTEP',
+         '-DGNUSTEP_BASE_LIBRARY=1',
+         '-DGNU_GUI_LIBRARY=1',
+         '-DGNU_RUNTIME=1',
+         '-DGNUSTEP_BASE_LIBRARY=1',
+         '-fno-strict-aliasing',
+         '-fexceptions',
+         '-fobjc-exceptions',
+         '-D_NATIVE_OBJC_EXCEPTIONS',
+         '-fPIC',
+         '-Wall',
+         '-DGSWARN',
+         '-DGSDIAGNOSE',
+         '-Wno-import',
+         '-g',
+         '-O2',
+         '-fstack-protector',
+         '--param=ssp-buffer-size=4',
+         '-D_FORTIFY_SOURCE=2',
+         '-Wformat',
+         '-Wformat-security',
+         '-Werror=format-security',
+         '-fgnu-runtime',
+         '-fconstant-string-class=NSConstantString',
+         '-I/usr/local/include/GNUstep',
+         '-I/usr/include/GNUstep',
+         '-lobjc',
+         '-lgnustep-base',
+         '-o',
+         compiler_file],
     apparmor_profile            = 'straitjacket/compiler/objective-c',
-    compiled_apparmor_profile   = 'straitjacket/compiled/default',
+    compiled_apparmor_profile   = 'straitjacket/compiled/objective-c',
     filename                    = 'source.m',)
     
 LanguageTest('test-simple', objective_c,
@@ -62,3 +95,25 @@ LanguageTest('test-includesafe', objective_c,
     stdout      = 'fatal error: /etc/hosts: Permission denied\n',
     returncode  = 1,
     status      = 'compilation failed')
+
+LanguageTest('test-stdin', objective_c,
+    source = (  '#import <Foundation/Foundation.h>                                                  \n'
+                'void readFromStdin()                                                               \n'
+                '{                                                                                  \n'       
+                '    NSFileHandle *stdin = [NSFileHandle fileHandleWithStandardInput];              \n'
+                '    NSData *data = [NSData dataWithData:[stdin readDataToEndOfFile]];              \n'
+                '    NSString *dataString = [[NSString alloc] initWithData:data                     \n'
+                '        encoding:NSUTF8StringEncoding];                                            \n'
+                '    [[NSFileHandle fileHandleWithStandardOutput]                                   \n'
+                '        writeData:[dataString dataUsingEncoding:NSUTF8StringEncoding]];            \n'
+                '}                                                                                  \n'
+                'int main(int argc, const char * argv[])                                            \n'
+                '{                                                                                  \n'
+                '    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];                   \n'
+                '    readFromStdin();                                                               \n'
+                '    [pool release];                                                                \n'
+                '    return 0;                                                                      \n'
+                '}'),
+    stdin       = 'hello\nworld',
+    stdout      = 'hello\nworld')
+    
